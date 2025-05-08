aef3635529d54d54463d4ccf616f3bfeff9af1e3
Vladimirs Nordholm <v@vladde.net>
2025-05-09 01:03:23 +0200

    feat(ci): command to resize images to max 1000px or max 300kb

--- .github/workflows/minimize-images.sh
+++ .github/workflows/minimize-images.sh
@@ -1,23 +1,54 @@
 #!/usr/bin/env bash
 
-JPG_COMMAND="-define jpeg:extent=300kb -resize 1000x -strip -interlace Plane -quality 85% -sampling-factor 4:2:0 -colorspace sRGB"
-PNG_COMMAND="-resize 1000x"
-WEBP_COMMAND="-resize 1000x -define webp:method=6 -define webp:target-size=300kb -define webp:quality=85 -define webp:auto-filter=0 -define webp:auto-level=0 -define webp:preprocessing=0 -define webp:dithering=0"
+set -euo pipefail
 
-for file in "$1"/*.jpg "$1"/*.jpeg "$1"/*.png "$1"/*.webp; do
-  if [ -f "$file" ]; then
-    width=$(identify -format "%w" "$file")
+for img in *.jpg; do
+  echo "optimizing $img"
 
-    if [[ "$width" -gt 1000 ]]; then
-      echo "Processing $file"
+  # works on both macOS and Linux
+  size=$( (stat -f%z "$img" 2>/dev/null || stat -c%s "$img" 2>/dev/null) | tr -d '[:space:]')
 
-      if [[ $file == *.jpg || $file == *.jpeg ]]; then
-        magick mogrify $JPG_COMMAND "$file"
-      elif [[ $file == *.png ]]; then
-        magick mogrify $PNG_COMMAND "$file"
-      elif [[ $file == *.webp ]]; then
-        magick mogrify $WEBP_COMMAND "$file"
-      fi
-    fi
+  # image less than ~300KB, no need to optimize
+  if [ "$size" -le 320000 ]; then
+    continue
   fi
+
+  magick mogrify -resize '1000x>' -strip \
+    -define jpeg:extent=300KB -sampling-factor 4:2:0 \
+    "$img"
+done
+
+for img in *.webp; do
+  echo "optimizing $img"
+
+  # works on both macOS and Linux
+  size=$( (stat -f%z "$img" 2>/dev/null || stat -c%s "$img" 2>/dev/null) | tr -d '[:space:]')
+
+  # image less than 300KB, no need to optimize
+  if [ "$size" -le 307200 ]; then
+    continue
+  fi
+
+  magick mogrify -resize '1000x>' -strip \
+    -define webp:lossless=false -quality 80 \
+    -define webp:target-size=300000 \
+    "$img"
+done
+
+for img in *.png; do
+  echo "optimizing $img"
+
+  # works on both macOS and Linux
+  size=$( (stat -f%z "$img" 2>/dev/null || stat -c%s "$img" 2>/dev/null) | tr -d '[:space:]')
+
+  # image less than 300KB, no need to optimize
+  if [ "$size" -le 307200 ]; then
+    continue
+  fi
+
+  magick mogrify -resize '1000x>' -strip -colors 256 \
+    -define png:compression-level=9 \
+    -define png:compression-filter=5 \
+    -define png:compression-strategy=1 \
+    "$img"
 done
